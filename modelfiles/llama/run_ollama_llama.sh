ac3d981c921976c6f16e9c673a641e9518e21057
Jose <jgarcia111@us.es>
2026-01-26 10:08:05 +0100

    Modelos slm - Los mas pequeños

--- modelfiles/llama/run_ollama_llama.sh
+++ modelfiles/llama/run_ollama_llama.sh
@@ -9,22 +9,22 @@ echo "Esperando 60 segundos a que el servidor esté listo..."
 sleep 60
 
 # Ejecutar creación de modelos
-echo "Iniciando creación de modelos LLAMA_8B..."
+echo "Iniciando creación de modelos LLAMA_1B..."
 
 # --- NO_RAG ---
-echo "Creando LLAMA_8B NO_RAG (T1, T2, T3)..."
-ollama create LLAMA_8B_LLM_resumen_NO_RAG_T1 -f ./llama/LLAMA_8B_LLM_resumir_NO_RAG_T1.txt
-ollama create LLAMA_8B_LLM_resumen_NO_RAG_T2 -f ./llama/LLAMA_8B_LLM_resumir_NO_RAG_T2.txt
-ollama create LLAMA_8B_LLM_resumen_NO_RAG_T3 -f ./llama/LLAMA_8B_LLM_resumir_NO_RAG_T3.txt
+echo "Creando LLAMA_1B NO_RAG (T1, T2, T3)..."
+ollama create LLAMA_1B_LLM_resumen_NO_RAG_T1 -f ./llama/LLAMA_1B_LLM_resumir_NO_RAG_T1.txt
+ollama create LLAMA_1B_LLM_resumen_NO_RAG_T2 -f ./llama/LLAMA_1B_LLM_resumir_NO_RAG_T2.txt
+ollama create LLAMA_1B_LLM_resumen_NO_RAG_T3 -f ./llama/LLAMA_1B_LLM_resumir_NO_RAG_T3.txt
 
 # --- RAG ---
-echo "Creando LLAMA_8B RAG (T1, T2, T3)..."
-ollama create LLAMA_8B_LLM_resumen_RAG_T1 -f ./llama/LLAMA_8B_LLM_resumir_RAG_T1.txt
-ollama create LLAMA_8B_LLM_resumen_RAG_T2 -f ./llama/LLAMA_8B_LLM_resumir_RAG_T2.txt
-ollama create LLAMA_8B_LLM_resumen_RAG_T3 -f ./llama/LLAMA_8B_LLM_resumir_RAG_T3.txt
+echo "Creando LLAMA_1B RAG (T1, T2, T3)..."
+ollama create LLAMA_1B_LLM_resumen_RAG_T1 -f ./llama/LLAMA_1B_LLM_resumir_RAG_T1.txt
+ollama create LLAMA_1B_LLM_resumen_RAG_T2 -f ./llama/LLAMA_1B_LLM_resumir_RAG_T2.txt
+ollama create LLAMA_1B_LLM_resumen_RAG_T3 -f ./llama/LLAMA_1B_LLM_resumir_RAG_T3.txt
 
 echo "¡Proceso finalizado! Lista de modelos actuales:"
-ollama list | grep LLAMA_8B_LLM_resumen
+ollama list | grep LLAMA_1B_LLM_resumen
 
 echo "Script de modelos finalizado."
 # ----------------------------------------------------
